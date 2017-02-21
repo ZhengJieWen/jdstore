84ada533c261e3cfef41cfe4d7ea0c22f4c535bd
rails103 <b96530111@gmail.com>
2017-02-21 16:25:43 +0800

    Revert "Index 的產品頁加了連結; Seeds.rb"

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,25 @@
 Rails.application.routes.draw do
-    root 'welcome#index'
-    devise_for :users
+  root 'welcome#index'
+  devise_for :users
+  namespace :admin do
+    resources :products
+  end
 
-    namespace :admin do
-        resources :products
-        resources :orders do
-        end
+  resources :products do
+    member do
+      post :add_to_cart
     end
-
-    resources :products do
-        member do
-            post :add_to_cart
-        end
+  end
+  resources :carts do
+    collection do
+      delete :clean
+      post :checkout
     end
+  end
 
-    resources :carts do
-        collection do
-            delete :clean
-            post :checkout
-        end
-    end
-
-    namespace :account do
-        resources :orders
-    end
-
-    resources :cart_items
-    resources :carts
+  resources :cart_items
+  resources :orders
+  namespace :account do
+    resources :orders
+  end
 end
