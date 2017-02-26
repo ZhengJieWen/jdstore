9a85c60a9e31f9fb4635aa26b321af14f57b458d
YoJuHsu <zick.dawz@Gmail.com>
2017-02-26 12:45:48 +0800

    revise routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,42 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  devise_for :users
-  namespace :admin do
-    resources :products
+    root 'welcome#index'
+    devise_for :users
+
+    namespace :admin do
+        resources :products
+        resources :orders do
+            member do
+                post :cancel
+                post :ship
+                post :shipped
+                post :return
+            end
+        end
+    end
+
+    resources :products do
+        member do
+            post :add_to_cart
+        end
+    end
+
+    resources :carts do
+        collection do
+            delete :clean
+            post :checkout
+        end
+    end
+
+    resources :cart_items
     resources :orders do
-      member do
-        post :cancel
-        post :ship
-        post :shipped
-        post :return
-      end
+        member do
+            post :pay_with_alipay
+            post :pay_with_wechat
+            post :apply_to_cancel
+        end
     end
-  end
 
-  resources :products do
-    member do
-      post :add_to_cart
+    namespace :account do
+        resources :orders
     end
-  end
-  resources :carts do
-    collection do
-      delete :clean
-      post :checkout
-    end
-  end
-
-  resources :cart_items
-  resources :orders do
-    member do
-      post :pay_with_alipay
-      post :pay_with_wechat
-      post :apply_to_cancel
-    end
-  end
-  namespace :account do
-    resources :orders
-  end
 end
